983042c1b73984e4a01e30712e6c3d9e568bfafb
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2023-01-21 11:49:27 -0300

    feat: basic config for styled-components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import styled from "styled-components/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled(View)`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled(Text)`
+  color: #000;
+  text-align: center;
+  font-size: 16px;
+`;
+export default () => (
+  <Container>
+    <Title>Basic Configuration with Styled Components</Title>
+    <StatusBar style="auto" />
+  </Container>
+);
