ff22d167a267b2b1a1a22be6052e06d96dc9f00d
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2023-02-04 07:55:03 -0300

    global components style

--- mobile/components/components.styled.tsx
+++ mobile/components/components.styled.tsx
@@ -1,7 +1,4 @@
 import { View } from "react-native";
 import styled from "styled-components/native";
 
-export const ScreenContainer = styled(View)`
-  flex: 1;
-  padding: 20px 20px;
-`;
+export const ComponentContainer = styled(View)``;
